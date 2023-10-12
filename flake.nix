08348e18cf8822801094c1e0369b5d7bc390cca8
Julius de Bruijn <julius+github@nauk.io>
2023-10-12 16:37:44 +0200

    tokio-postgres

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Rustls Postgres Tokio Boom Chaka Laka";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [pkgs.bashInteractive];
+        buildInputs = with pkgs; [
+          rustup
+        ];
+      };
+    });
+}
